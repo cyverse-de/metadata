50691a6fdf9165a4504548766ef2745bd2fa10f2
Paul Sarando <psarando@iplantcollaborative.org>
2015-06-19 18:27:15 -0700

    CORE-6867 Moved service Swagger docs under /docs endpoint.
    Moved the Swagger docs for the data-info, metadata, and metadactyl services to a /docs endpoint.
    Moved the /status endpoints for data-info and metadata to their root "/" endpoints.
    Added a clojure-commons.service namespace that returns a common service status info map.
    Added a metadactyl root "/" status endpoint.

--- src/metadata/routes/domain/status.clj
+++ src/metadata/routes/domain/status.clj
@@ -6,4 +6,5 @@
 (s/defschema StatusResponse
   {:service     (describe NonBlankString "The name of the service")
    :description (describe NonBlankString "The service description")
-   :version     (describe NonBlankString "The service version")})
+   :version     (describe NonBlankString "The service version")
+   :docs-url    (describe NonBlankString "The service API docs")})
--- src/metadata/routes/status.clj
+++ src/metadata/routes/status.clj
@@ -1,14 +1,17 @@
 (ns metadata.routes.status
   (:use [compojure.api.sweet]
         [metadata.routes.domain.status])
-  (:require [metadata.util.service :as service]))
+  (:require [clojure-commons.service :as commons-svc]
+            [metadata.util.config :as config]
+            [metadata.util.service :as service]))
 
 (defroutes* status
-  (context* "/status" []
+  (context* "/" []
     :tags ["service-info"]
 
-    (GET* "/" [:as {uri :uri}]
+    (GET* "/" [:as {:keys [uri server-name server-port]}]
       :return StatusResponse
       :summary "Service Information"
       :description "This endpoint provides the name of the service and its version."
-      (service/trap uri service/get-status))))
+      (service/trap uri
+        commons-svc/get-docs-status config/svc-info server-name server-port config/docs-uri))))

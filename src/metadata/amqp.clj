a0dedbaaee8c72c8cac9c410316e5d810c2d2093
Jonathan Strootman <jstroot@iplantcollaborative.org>
2016-09-18 11:00:41 -0700

    Add AMQP vhost cfg property to AMQP connection
    CORE-8309
    Resolves: CORE-8317

--- src/metadata/amqp.clj
+++ src/metadata/amqp.clj
@@ -8,11 +8,8 @@
 
 (defn publish-metadata-update
   [user id]
-  (let [conn  (rmq/connect {:host                (config/amqp-host)
-                            :port                  (config/amqp-port)
-                            :username              (config/amqp-user)
-                            :password              (config/amqp-pass)})
-        ch    (lch/open conn)
+  (let [conn (rmq/connect {:uri (config/amqp-uri)})
+        ch (lch/open conn)
         ename (config/amqp-exchange)]
     (le/declare ch ename "direct" {:durable true})
     (lb/publish ch ename "metadata.update" (json/encode {:entity id :author user}) {:content-type "application/json" :persistent 1})

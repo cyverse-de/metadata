457eb97a73427c0769cf22c2f55d60c177a92116
Sarah Roberts <sarahr@cyverse.org>
2019-03-26 18:02:27 -0700

    CORE-145: fixed a failing unit test

--- test/metadata/util/config_test.clj
+++ test/metadata/util/config_test.clj
@@ -8,7 +8,7 @@
     (config/load-config-from-file "conf/test/empty.properties")
     (is (= (config/listen-port) 60000))
     (is (= (config/environment-name) "docker-compose"))
-    (is (= (config/db-subprotocol) "pgsql"))
+    (is (= (config/db-subprotocol) "postgresql"))
     (is (= (config/db-host) "dedb"))
     (is (= (config/db-port) "5432"))
     (is (= (config/db-name) "metadata"))

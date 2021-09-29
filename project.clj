b7e66f5aa4aab90322ad3f9d6b73512403cdc50d
Paul Sarando <psarando@iplantcollaborative.org>
2021-09-28 20:04:18 -0700

    CORE-11 Migrate permanent-id-request endpoint schemas and docs
    Migrated metadata.routes.permanent-id-requests docs
    and metadata.routes.schemas.permanent-id-requests schemas
    to common-swagger-api.schema.permanent-id-requests.

--- project.clj
+++ project.clj
@@ -28,7 +28,7 @@
                  [org.cyverse/clojure-commons "3.0.6"]
                  [org.cyverse/common-cfg "2.8.1"]
                  [org.cyverse/common-cli "2.8.1"]
-                 [org.cyverse/common-swagger-api "3.0.7"]
+                 [org.cyverse/common-swagger-api "3.2.1"]
                  [org.cyverse/kameleon "3.0.4"]
                  [org.cyverse/service-logging "2.8.2"]
                  [org.cyverse/event-messages "0.0.1"]
